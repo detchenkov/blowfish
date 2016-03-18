488d7ad71221f58dbce2336f1f23a9718f370d50
detchenkov <detchenkov@bk.ru>
2016-03-18 11:15:09 +0800

    initial commit

---
+++ blowfish.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   blowfish.h
+ * Author: ilia
+ *
+ * Created on 18 марта 2016 г., 11:08
+ */
+
+#ifndef BLOWFISH_H
+#define BLOWFISH_H
+
+
+
+#endif /* BLOWFISH_H */
+
